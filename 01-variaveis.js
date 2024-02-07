c69b687280471a541c295e8c951dfe17dcf2790d
Beatriz <charlottebeatrizn@gmail.com>
2024-02-07 11:26:13 -0300

    javascript - basico

---
+++ 01-variaveis.js
@@ -0,0 +1,11 @@
+//Criando variaveis com "var" (var nome_variavel = <valor>;)
+
+var nome = "Beatriz"; //var cria variavel global
+var idade = 16;
+
+console.log("Meu nome é", nome, "e tenho", idade, "anos!");
+console.log("Agora fiz aniversario😊 Tenho", idade + 1, "anos!");
+
+//aumentando automagicamente +1 (autoincremento)
+idade++ //idade = idade + 1;
+console.log("Agora fiz aniversario😊 Tenho", idade, "anos!");
