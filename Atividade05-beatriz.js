ee9da606455ce66993ff45752b7623803bd3e8e0
Beatriz <charlottebeatrizn@gmail.com>
2024-02-28 10:22:39 -0300

    Função

---
+++ Atividade05-beatriz.js
@@ -0,0 +1,14 @@
+function idade(ano) {
+  return 2024 - ano;
+}
+console.log(`A clara tem ${idade(2010)} anos.`);
+
+function quantidade(combustivel, kms) {
+  return combustivel * kms;
+}
+console.log(
+  `O carro de Claudia com 1 litro de combustivel pode percorrer ${quantidade(
+    1,
+    12
+  )} km`
+);
